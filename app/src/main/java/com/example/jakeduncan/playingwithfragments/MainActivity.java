3cd4ba4c3c4550798d8c6f8bfd890a2f1be20b31
Jaked222 <Asphynx222@gmail.com>
2016-10-14 10:45:49 -0400

    just have two plain fragments on a main activity. soon to make them interchange during runtime

---
+++ app/src/main/java/com/example/jakeduncan/playingwithfragments/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jakeduncan.playingwithfragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_articles);
+    }
+}
