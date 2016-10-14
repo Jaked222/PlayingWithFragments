5870a6586d75d7d95fe85b78a73ff1e610fd3d73
Jaked222 <Asphynx222@gmail.com>
2016-10-14 11:10:02 -0400

    fragments working as expected but button is weird.

--- app/src/main/java/com/example/jakeduncan/playingwithfragments/MainActivity.java
+++ app/src/main/java/com/example/jakeduncan/playingwithfragments/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.jakeduncan.playingwithfragments;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_articles);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HeadlineFragment firstFragment = new HeadlineFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+        this.button = (Button) this.findViewById(R.id.button);
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create fragment and give it an argument specifying the article it should show
+                ArticleFragment newFragment = new ArticleFragment();
+            //    Bundle args = new Bundle();
+             //   args.putInt(ArticleFragment.ARG_POSITION, position);
+            //    newFragment.setArguments(args);
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack so the user can navigate back
+                transaction.replace(R.id.fragment_container, newFragment);
+                transaction.addToBackStack(null);
+
+// Commit the transaction
+                transaction.commit();
+            }
+        });
     }
 }
